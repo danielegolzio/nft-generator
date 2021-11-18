018218c0f276a3be0bc88ba95c5cd6b72014c782
dan <80271815+poonchoi@users.noreply.github.com>
2021-11-18 22:36:25 +0100

    dino list

---
+++ nft-generator.py
@@ -0,0 +1,22 @@
+dino = [
+    ['bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','bg','bg','bg','ot','ot','ot','bg','bg','bg','bg','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','bg','ot','ot','bc','bc','bc','ot','ot','bg','bg','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','ot','bc','bc','ec','ew','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','ot','bc','bc','bc','ec','ec','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','ot','ot','ot','ot','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg'],
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+    ['bg','bg','bg','bg','bg','bg','bg','bg','ot','bc','bc','bc','bc','bc','bc','ot','bg','bg','bg','bg']
+]
