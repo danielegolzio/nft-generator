2a1d0ccb5c2bf0c7d07b03ff51cc371c1a5bd9bb
dan <80271815+poonchoi@users.noreply.github.com>
2021-11-20 18:40:15 +0100

    ...

--- turns-image-into-ascii.py
+++ turns-image-into-ascii.py
@@ -14,30 +14,36 @@ def image_data_file(data):
     file = open('image_data.txt', 'w')
 
     for i in range(30):
-
         for j in range(30):
+
             R, G, B = data[i][j]
+
             if (R,G,B) == (0, 0, 0):
-                file.write('PC,')
+                file.write('PF')
 
             elif (R,G,B) == (255, 255, 255):
-                file.write('EW,')
+                file.write('EW')
 
             elif (R,G,B) == (255, 0, 0):
-                file.write('EC,')
+                file.write('EC')
 
             elif (R,G,B) == (200, 200, 200):
-                file.write('BC,')
+                file.write('BC')
 
             elif (R,G,B) == (50, 50, 50):
-                file.write('OT,')
+                file.write('OT')
             
             elif (R,G,B) == (0, 0, 255):
-                file.write('BG,')
+                file.write('BG')
             elif (R,G,B) == (0, 255, 0):
-                file.write('BK,')
-        file.write('\n')
+                file.write('BK')
 
+            if i != 30 and j != 29:
+                file.write(',')
+
+        if i != 30 and j != 30:
+            file.write('\n')
+        
     file.close()
 
 
