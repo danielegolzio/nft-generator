158cde611f67ee7a673bb4ecaa8710d283ef61e8
dan <80271815+poonchoi@users.noreply.github.com>
2021-11-20 15:53:08 +0100

    Update nft-generator.py

--- nft-generator.py
+++ nft-generator.py
@@ -1,16 +1,15 @@
 def image_file_to_array():
-    image_array = []
+    array = []
 
     file = open('image_data.txt', 'r')
 
-    file.strip(" ")
     lines = file.readlines()
     for line in lines:
-        image_array = image_array.append(line.split(","))
+        array = array.append(line.split(","))
 
     file.close()
     
-    print(image_array)
+    print(array)
 
 if __name__ == '__main__':
     image_file_to_array()
