f3f0daf7a76dcd670a9b724d8db874d5dc4c73d2
dan <80271815+poonchoi@users.noreply.github.com>
2021-11-24 11:36:51 +0100

    Update art-generator.py

--- art-generator.py
+++ art-generator.py
@@ -1,17 +1,38 @@
 import numpy as np
 import random
+from PIL import Image
 
 def file_to_array():
     file = open('image_data.txt', 'r')
     data = [line.strip('\n')[:-1].split(',') if line[-2] == "," else line.strip('\n').split(',') for line in file.readlines()]
     file.close()
     
-    color_generator(data)
+    rarity(data)
     return data
 
-def color_generator(data):
-    pf = [0, 0, 0]
+
+def rarity(data, rarity):
+    number = random.randint(1, 1000)
+    if number >= 1 and number <= 500: # 50% chance of getting this rarity
+        rarity = 'common'
     
+    if number >= 500 and number <= 750: # 25% chance of getting this rarity
+        rarity = 'uncommon'
+
+    if number >= 750 and number <= 850: # 10 % chance of getting this rarity
+        rarity = 'rare'
+    
+    
+
+    art_generator(data, rarity)
+    return rarity
+
+
+def art_generator(data, rarity):
+    for i in 50: # main loop for generating art
+        pass
+
+
 
 if __name__ == '__main__':
     file_to_array()
