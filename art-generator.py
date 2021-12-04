cfecf370470c112230ed772ab37646233b56462f
dan <80271815+poonchoi@users.noreply.github.com>
2021-12-04 22:44:11 +0100

    more color generation

--- art-generator.py
+++ art-generator.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import random
 from PIL import Image
@@ -13,51 +12,105 @@ def file_to_array(): # turns image_data file into a list
     rarity(data)
     return data
 
+################################################################################################################################################
 
 def rarity(data): # generates rarity for image
     number = random.randint(1, 1000)
+
     if number >= 1 and number <= 500: # 50% chance of getting this rarity
         rarity = 'common'
+        common()
     
     elif number >= 500 and number <= 750: # 25% chance of getting this rarity
         rarity = 'uncommon'
+        uncommon()
 
     elif number >= 750 and number <= 850: # 10% chance of getting this rarity
         rarity = 'rare'
-    
+        rare()
+
     elif number >= 850 and number <= 900: # 5% chance of getting this rarity
         rarity = 'covert'
-    
+        covert()
+
     elif number >= 950 and number <= 960: # 1% chance of getting this rarity
         rarity = 'legendary'
         main_colors = 'red'
+        legendary_r()
 
     elif number >= 960 and number <= 970: # 1% chance of getting this rarity
         rarity = 'legendary'
         main_colors = 'green'
+        legendary_g()
 
     elif number >= 970 and number <= 980: # 1% chance of getting this rarity
         rarity = 'legendary'
         main_colors = 'blue'
+        legendary_b()
 
     elif number == 999: # 0.1% chance of getting this rarity
         rarity = 'classified'
         main_colors = 'black'
-    
+        classified_blk()
+
     elif number == 1000: # 0.1% chance of getting this rarity
         rarity = 'classified'
         main_colors = 'white'
-
+        classified_wht()
 
     art_generator(data, rarity)
     return rarity
 
+################################################################################################################################################
+
+def common():
+    PF = [0,0,0] 
+    PF = np.array(PF)
+    EW = [255,255,255]
+    EW = np.array(EW)
+    EC = [random.randint(0, 250),random.randint(0, 250),random.randint(0, 250)]
+    EC = np.array(EC)
+    BC = [random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)]
+    BC = np.array(BC)
+    OT = [random.randint(100, 250),random.randint(100, 250),random.randint(100, 250)]
+    OT = np.array(OT)
+    BG = [250,249,213]
+    BG = np.array(BG)
+    BK = [random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)]
+    BK = np.array(BK)
+    return PF, EW, EC, BC, OT, BG, BK
+
+def uncommon():
+    pass
+
+def rare():
+    pass
+
+def covert():
+    pass
+
+def legendary_r():
+    pass
+
+def legendary_b():
+    pass
+
+def legendary_g():
+    pass
+
+def classified_blk():
+    pass
+
+def classified_wht():
+    pass
+
+################################################################################################################################################
 
 def art_generator(data, rarity):
     for i in range(50): # main loop for generating art
         pass
 
-
+################################################################################################################################################
 
 if __name__ == '__main__':
     file_to_array()
---
+++ color_generator.py
@@ -0,0 +1,49 @@
+# generates the colors depending on the rarity
+# then it turns the python list into a numpy array
+
+import random as r
+import numpy
+
+def common():
+    PF = [0,0,0] 
+    PF = numpy.array(PF)
+    EW = [255,255,255]
+    EW = numpy.array(EW)
+    EC = [r.randint(0, 250),r.randint(0, 250),r.randint(0, 250)]
+    EC = numpy.array(EC)
+    BC = [r.randint(0, 255),r.randint(0, 255),r.randint(0, 255)]
+    BC = numpy.array(BC)
+    OT = [r.randint(100, 250),r.randint(100, 250),r.randint(100, 250)]
+    OT = numpy.array(OT)
+    BG = [250,249,213]
+    BG = numpy.array(BG)
+    BK = [r.randint(0, 255),r.randint(0, 255),r.randint(0, 255)]
+    BK = numpy.array(BK)
+    return PF, EW, EC, BC, OT, BG, BK
+
+def uncommon():
+    pass
+
+def rare():
+    pass
+
+def covert():
+    pass
+
+def legendary_r():
+    pass
+
+def legendary_b():
+    pass
+
+def legendary_g():
+    pass
+
+def classified_blk():
+    pass
+
+def classified_wht():
+    pass
+
+if __name__ == '__main__':
+    common()
