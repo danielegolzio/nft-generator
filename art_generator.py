489d56ceb3386f7d8baf536db37ddd4aebed22e4
dan <danielegolzio@gmail.com>
2021-12-07 11:37:02 +0100

    no new additions... yet

--- art_generator.py
+++ art_generator.py
@@ -36,66 +36,66 @@ def main_loop():
 
         # rarity generator
         def rarity(data):
-            # number = random.randint(1, 1000)
+            number = random.randint(1, 1000)
             
-            # # 50% chance of getting this rarity
-            # if number >= 1 and number <= 500: 
-            #     rarity = 'common'
-            #     common(data, rarity)
-            #     return rarity
+            # 50% chance of getting this rarity
+            if number >= 1 and number <= 500: 
+                rarity = 'common'
+                common(data, rarity)
+                return rarity
             
-            # # 25% chance of getting this rarity
-            # elif number >= 500 and number <= 750: 
-            #     rarity = 'uncommon'
-            #     uncommon(data, rarity)
-            #     return rarity
+            # 25% chance of getting this rarity
+            elif number >= 500 and number <= 750: 
+                rarity = 'uncommon'
+                uncommon(data, rarity)
+                return rarity
 
-            # # 10% chance of getting this rarity
-            # elif number >= 750 and number <= 850: 
-            #     rarity = 'rare'
-            #     rare(data, rarity)
-            #     return rarity
+            # 10% chance of getting this rarity
+            elif number >= 750 and number <= 850: 
+                rarity = 'rare'
+                rare(data, rarity)
+                return rarity
 
-            # # 5% chance of getting this rarity
-            # elif number >= 850 and number <= 900: 
-            #     rarity = 'covert'
-            #     covert(data, rarity)
-            #     return rarity
+            # 5% chance of getting this rarity
+            elif number >= 850 and number <= 900: 
+                rarity = 'covert'
+                covert(data, rarity)
+                return rarity
 
-            # # 1% chance of getting this rarity
-            # elif number >= 950 and number <= 960: 
-            #     rarity = 'legendary'
-            #     main_colors = 'red'
-            #     legendary_r(data, rarity)
-            #     return rarity
+            # 1% chance of getting this rarity
+            elif number >= 950 and number <= 960: 
+                rarity = 'legendary'
+                main_colors = 'red'
+                legendary_r(data, rarity)
+                return rarity
 
-            # # 1% chance of getting this rarity
-            # elif number >= 960 and number <= 970: 
-            #     rarity = 'legendary'
-            #     main_colors = 'green'
-            #     legendary_g(data, rarity)
-            #     return rarity
+            # 1% chance of getting this rarity
+            elif number >= 960 and number <= 970: 
+                rarity = 'legendary'
+                main_colors = 'green'
+                legendary_g(data, rarity)
+                return rarity
 
-            # # 1% chance of getting this rarity
-            # elif number >= 970 and number <= 980: 
-            #     rarity = 'legendary'
-            #     main_colors = 'blue'
-            #     legendary_b(data, rarity)
-            #     return rarity
+            # 1% chance of getting this rarity
+            elif number >= 970 and number <= 980: 
+                rarity = 'legendary'
+                main_colors = 'blue'
+                legendary_b(data, rarity)
+                return rarity
 
-            # # 0.1% chance of getting this rarity
-            # elif number == 999: 
-            #     rarity = 'classified'
-            #     main_colors = 'black'
-            classified_blk(data, rarity)
-            #     return rarity
+            # 0.1% chance of getting this rarity
+            elif number == 999: 
+                rarity = 'classified'
+                main_colors = 'black'
+                classified_blk(data, rarity)
+                return rarity
 
-            # # 0.1% chance of getting this rarity
-            # elif number == 1000: 
-            #     rarity = 'classified'
-            #     main_colors = 'white'
-            #     classified_wht(data, rarity)
-            #     return rarity
+            # 0.1% chance of getting this rarity
+            elif number == 1000: 
+                rarity = 'classified'
+                main_colors = 'white'
+                classified_wht(data, rarity)
+                return rarity
             
 
 
