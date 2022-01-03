9e4196d498e21910749ee44c201328b4e6b98fbb
dan <danielegolzio@gmail.com>
2022-01-03 17:11:17 +0100

    FIX: makes colors more varied

--- image-generation-accessories/colors.py
+++ image-generation-accessories/colors.py
@@ -6,8 +6,16 @@ def color_gen():
     PF = [0,0,0]
     EW = [255,255,255]
     EC = [0,0,0]
-    BC = [random.randint(70, 210),random.randint(70, 210),random.randint(70, 210)]
-    BG = [255-BC[0],255-BC[1],255-BC[2]]
+    
+    # this makes the colors more varied
+    prob = random.choice([True, False])
+    if prob:
+        BC = [random.randint(70, 210),random.randint(70, 210),random.randint(70, 210)]
+        BG = [255-BC[0],255-BC[1],255-BC[2]]
+    else:
+        BG = [random.randint(70, 210),random.randint(70, 210),random.randint(70, 210)]
+        BC = [255-BG[0],255-BG[1],255-BG[2]]
+        
     OT = [50,50,50]
     BK = [255,198,110]
     
