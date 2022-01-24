00194cbe30082bcbd0bd16825da9ab250d1d768a
dan <80271815+poonchoi@users.noreply.github.com>
2022-01-24 12:05:04 +0100

    FIX: removed unused files

--- v2/imggen/colorgen/colors.py
+++ v2/imggen/colorgen/colors.py
@@ -8,10 +8,10 @@ def color_gen():
     # this makes the colors more varied
     prob = random.choice([True, False])
     if prob:
-        BC = [random.randint(30, 220),random.randint(30, 220),random.randint(30, 220)]
+        BC = [random.randint(30, 210),random.randint(30, 210),random.randint(30, 210)]
         BG = [255-BC[0],255-BC[1],255-BC[2]]
     else:
-        BG = [random.randint(30, 220),random.randint(30, 220),random.randint(30, 220)]
+        BG = [random.randint(30, 210),random.randint(30, 210),random.randint(30, 210)]
         BC = [255-BG[0],255-BG[1],255-BG[2]]
         
     OT = [50,50,50]
