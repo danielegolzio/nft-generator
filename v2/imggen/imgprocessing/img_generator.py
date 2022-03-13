454c4dc4fa9ed8c7ebc296bad0091dd1af332d77
dan <80271815+poonchoi@users.noreply.github.com>
2022-03-13 21:48:20 +0100

    Update img_generator.py

--- v2/imggen/imgprocessing/img_generator.py
+++ v2/imggen/imgprocessing/img_generator.py
@@ -3,20 +3,21 @@ import numpy as np
 import cv2
 from imggen.dimensions.dimensions import dimensions
 
+
 def img_generator(BC, BG, im):
     """
     Generates the standard duck image with no accessories
     """
     new_img = im.copy()
-    new_img[np.where((new_img==(255,0,0)).all(axis=2))] = BG
-    new_img[np.where((new_img==(0,255,0)).all(axis=2))] = BC
+    new_img[np.where((new_img == (255, 0, 0)).all(axis=2))] = BG
+    new_img[np.where((new_img == (0, 255, 0)).all(axis=2))] = BC
 
     # array handling with numpy
     new_img = np.array(new_img, dtype=np.uint8)
-    new_img = new_img.reshape(30,30,3)
+    new_img = new_img.reshape(30, 30, 3)
 
     # using PIL to turn the RGB values into an image
-    img_data = Image.fromarray(new_img, 'RGB')
+    img_data = Image.fromarray(new_img, "RGB")
     img_data = img_data.resize(dimensions, resample=0)
 
     return img_data
