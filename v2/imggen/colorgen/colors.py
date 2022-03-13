03df7d11b35dc890fb9368cfd38fc5a3f454066a
dan <80271815+poonchoi@users.noreply.github.com>
2022-03-13 21:48:27 +0100

    Update colors.py

--- v2/imggen/colorgen/colors.py
+++ v2/imggen/colorgen/colors.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 
+
 def color_gen():
     """
     Generates the color of the body and background
@@ -8,10 +9,18 @@ def color_gen():
     # this makes the colors a little more varied
     prob = np.random.choice([True, False])
     if prob:
-        BC = (np.random.randint(30, 210),np.random.randint(30, 210),np.random.randint(30, 210))
-        BG = (255-BC[0],255-BC[1],255-BC[2])
+        BC = (
+            np.random.randint(30, 210),
+            np.random.randint(30, 210),
+            np.random.randint(30, 210),
+        )
+        BG = (255 - BC[0], 255 - BC[1], 255 - BC[2])
     else:
-        BG = (np.random.randint(30, 210),np.random.randint(30, 210),np.random.randint(30, 210))
-        BC = (255-BG[0],255-BG[1],255-BG[2])
-        
+        BG = (
+            np.random.randint(30, 210),
+            np.random.randint(30, 210),
+            np.random.randint(30, 210),
+        )
+        BC = (255 - BG[0], 255 - BG[1], 255 - BG[2])
+
     return BC, BG
