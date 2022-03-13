22ca82381e601f2c512fe66857203d6245792f6a
dan <80271815+poonchoi@users.noreply.github.com>
2022-03-13 21:48:30 +0100

    Update progressbar.py

--- v2/imggen/args/progressbar/progressbar.py
+++ v2/imggen/args/progressbar/progressbar.py
@@ -4,7 +4,13 @@ import typer
 bar_empty = "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
 bar_block = ""
 
+
 def progressbar(bar_counter, loop_counter):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    bar_block = ("█"*(round(int((bar_counter/loop_counter)*30), 1)))
-    typer.secho("|" + bar_block + (bar_empty[len(bar_block)::]) + f"| [{bar_counter}/{loop_counter}] [{int((bar_counter/loop_counter)*100)}%]")
+    os.system("cls" if os.name == "nt" else "clear")
+    bar_block = "█" * (round(int((bar_counter / loop_counter) * 30), 1))
+    typer.secho(
+        "|"
+        + bar_block
+        + (bar_empty[len(bar_block) : :])
+        + f"| [{bar_counter}/{loop_counter}] [{int((bar_counter/loop_counter)*100)}%]"
+    )
